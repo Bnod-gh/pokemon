94821724980479d9af7818d016e9591309112359
Bnod-gh <128970084+Bnod-gh@users.noreply.github.com>
2024-08-06 14:00:01 +0930

    Update PokemonFetcher.jsx
    Added limit

--- src/assets/Component/PokemonFetcher.jsx
+++ src/assets/Component/PokemonFetcher.jsx
@@ -5,7 +5,7 @@ function PokemonFetcher({ getPokemon ,pokemonsearch}) {
   const [loading, setLoading] = useState(true);
   const [searchPokemon,setSearchPokemon]=([])
   const [error,setError]=useState('')
-  const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20";
+  const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=1000&offset=20";
 
   const getPokemon_url = async () => {
     try {
